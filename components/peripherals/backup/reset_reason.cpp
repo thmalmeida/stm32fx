3c4c718a6328e1e764a8616f1bf8f39d1d9f1479
thmalmeida <thmalmeida@gmail.com>
2023-03-28 06:40:09 -0300

    reset reason more simple

--- components/peripherals/backup/reset_reason.cpp
+++ components/peripherals/backup/reset_reason.cpp
@@ -3,47 +3,42 @@
 static uint8_t reset_reason_read_flag = 0;
 static reset_reason_t reset_reason_last = reset_reason_t::UNKNOW;
 
-reset_reason_t reset_reason(void) {
-	reset_reason_t r;
-
-	// uint32_t r0 = (RCC->CSR >> 26);
-	if(reset_reason_read_flag) {
-		return reset_reason_last;
-	} else {
-		if (__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST)) {
-			r = reset_reason_t::LOW_POWER_RESET;
-		}
-		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST)) {
-			r = reset_reason_t::WINDOW_WATCHDOG_RESET;
-		}
-		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) {
-			r = reset_reason_t::INDEPENDENT_WATCHDOG_RESET;
-		}
-		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST)) {
-			r = reset_reason_t::SOFTWARE_RESET;
-		}
-		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST)) {
-			r = reset_reason_t::POWER_ON_POWER_DOWN_RESET;
-		}
-		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST)) {
-			r = reset_reason_t::EXTERNAL_RESET_PIN_RESET;
-		}
-		else {
-			r = reset_reason_t::UNKNOW;
-		}
-
-		reset_reason_read_flag = 1;
-	}
-	reset_clear_flags();
-
-	return r;
-}
-void reset_clear_flags(void) {
+static void reset_reason_clear_flags(void) {
 	// set RMVF bit 24;
-	RCC->CSR |= (1<<24);
-
+	// RCC->CSR |= (1<<24);
 	// or
 	// Clear all the reset flags or else they will remain set during future
 	// resets until system power is fully removed.
 	__HAL_RCC_CLEAR_RESET_FLAGS();
+}
+reset_reason_t reset_reason(void) {
+	// All theses flags is located at RCC->CSR register
+	if(!reset_reason_read_flag) {
+		if (__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST)) {
+			reset_reason_last = reset_reason_t::LOW_POWER_RESET;
+		}
+		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST)) {
+			reset_reason_last = reset_reason_t::WINDOW_WATCHDOG_RESET;
+		}
+		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) {
+			reset_reason_last = reset_reason_t::INDEPENDENT_WATCHDOG_RESET;
+		}
+		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST)) {
+			reset_reason_last = reset_reason_t::SOFTWARE_RESET;
+		}
+		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST)) {
+			reset_reason_last = reset_reason_t::POWER_ON_POWER_DOWN_RESET;
+		}
+		else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST)) {
+			reset_reason_last = reset_reason_t::EXTERNAL_RESET_PIN_RESET;
+		}
+		else {
+			reset_reason_last = reset_reason_t::UNKNOW;
+		}
+
+		reset_reason_read_flag = 1;
+		reset_reason_clear_flags();
+	}
+
+	return reset_reason_last;
 }
