ea43c8830f2c874e93d0c4f9e8b7a27ef1b59b74
thmalmeida <thmalmeida@gmail.com>
2024-01-30 22:02:40 -0300

    getting better

--- components/systems/weighing_controller/include/weighing_scale.h
+++ components/systems/weighing_controller/include/weighing_scale.h
@@ -1,7 +1,7 @@
 #ifndef __WEIGHING_SCALE_H__
 #define __WEIGHING_SCALE_H__
 
-#include "load_cell.h"
+#include <load_cell.h>
 
 /* This class defines the controller of weighing scale. It uses each load cell as an object with it's parameters.
 * 
@@ -12,26 +12,77 @@
 * Debug and find erros on each load cell
 * print to LCD?
 *
+* Initial weight of cattle contention of each load cell (foot) (Iracema's Farm)
+* P1:  420.41 kg
+* P2:  340.86 kg
+* P3:  401.14 kg
+* P4:  360.24 kg
+* Pt: 1522.65 kg
 */
+
+/* Load cell pins definition */
+#define PIN_DATA_1	6
+#define PIN_SCK_1	7
+#define PIN_DATA_2	31
+#define PIN_SCK_2	32
+#define PIN_DATA_3	24
+#define PIN_SCK_3	23
+#define PIN_DATA_4	22
+#define PIN_SCK_4	21
+
+// #define pin_tare	32
+// #define pin_led		2
+// #define pin_beep	8
 
 class WEIGHING_SCALE {
 public:
 
+	WEIGHING_SCALE(void) : load_cell_{{PIN_DATA_1, PIN_SCK_1}, {PIN_DATA_2, PIN_SCK_2}, {PIN_DATA_3, PIN_SCK_3}, {PIN_DATA_4, PIN_SCK_4}} {
+		init();
+	}
 	WEIGHING_SCALE(int pin_data_1, int pin_sck_1,
 					int pin_data_2, int pin_sck_2,
 					int pin_data_3, int pin_sck_3,
-					int pin_data_4, int pin_sck_4) : load_cell_{{pin_data_1, pin_sck_1}, {pin_data_2, pin_sck_2}, {pin_data_3, pin_sck_3}, {pin_data_4, pin_sck_4}} {
+					int pin_data_4, int pin_sck_4) :
+						load_cell_{
+							{pin_data_1, pin_sck_1},
+							{pin_data_2, pin_sck_2},
+							{pin_data_3, pin_sck_3},
+							{pin_data_4, pin_sck_4}} {
 		init();
 	}
 
 	void init(void) {
 
-	}
+		// Load cell sensor parameters definition
+		// A[0] = 1.0000;			// 1kg load bar (small load cell sensor)
+		A_[0] = 2.9997;			// s1
+		A_[1] = 3.0000;			// s2
+		A_[2] = 3.0017;			// s3
+		A_[3] = 3.0012;			// s4
 
+		// Kc[0] = 10*1.3629;		// proportional constant of small 1kg load bar sensor
+		Kc_[0] = 1.0872;
+		Kc_[1] = 1.6200;			// YZC-320 tested on 20170816 with 3.0mV/V and Kc = 1.6985;
+		Kc_[2] = 1.0872;			//10.33%
+		Kc_[3] = 1.0872;
+
+		offset_[0] = 11500;	// s1
+		offset_[1] = 5000;	// s2
+		offset_[2] = 11500; // s3
+		offset_[3] = 11500;	// s4
+
+		for(int i=0; i<4; i++) {
+			load_cell_[i].offset(offset_[i]);
+		}
+
+	}
 	void run(void) {
 		
 	}
-
+	void weight(void) {
+		// load_cell_[0].
+	}
     void tare_system3(void) {
 		// int Ssum = 0;
 		// for(int i=0; i<nWeight; i++) {
@@ -41,6 +92,11 @@ public:
 	}
 private:
 	LOAD_CELL load_cell_[4];
+
+	/* Load cell parameters */
+	double A_[4], Kc_[4];
+	uint32_t offset_[4];
+
 };
 
 #endif //__WEIGHING_SCALE_H__
--- components/systems/weighing_controller/load_cell/include/load_cell.h
+++ components/systems/weighing_controller/load_cell/include/load_cell.h
@@ -12,16 +12,9 @@
 #include "hx711.h"
 #include "math.h"
 
-//#define pin_data_HX711	32
-//#define pin_sck_HX711	31
-// #define pin_tare_HX711	32
-// #define pin_led			2
-// #define pin_beep		8
-
 class LOAD_CELL {
 public:
 
-	static const int nWeight = 50;			// number of samples to count into average summation;
 	int stabWeight = 501;		//
 	int unstWeight = 25000;	//
 
@@ -30,20 +23,16 @@ public:
 
 	int WeightIndex = 0;
 
-	uint8_t pin_data_HX711;
-	uint8_t pin_sck_HX711;
-
 	uint8_t stable = 0;
 
-	int timeD = 1;							// time/frequency of HX711 interface;
-	int offset = 0;							// offset after tare;
 	int Weight = 0;							// currently weight x10;
-
 	int P;									// currently weight found;
 	int convCount;							// counter to fast convergence find;
 
+	static const int nWeight = 50;			// number of samples to count into average summation;
 	int WeightVect[nWeight];				// vector of weights calculated;
 	int signalVect[nWeight];				// digital values obtained from HX711 24 bits;
+	
 	int signal;
 	int error;
 
@@ -77,14 +66,13 @@ public:
 		A = _A;
 		Kp = _Kp;
 	}
-	int readInput();
-	void pin_sck_set(uint8_t status);
-	void pin_data_set(uint8_t status);
-	uint32_t pin_data_get();
-	float weight(void) {
+	int read_hx711() {
+		return hx711_.read();
+	}
+	int weight_kg(void) {
 
-		signal = readInput();						// Read the ADC channel;
-		double Vdig = (signal - offset);			// Remove the offset on pure signal;
+		signal = read_hx711();						// Read the ADC channel from HX711 module;
+		double Vdig = (signal - offset_);			// Remove the offset on pure signal;
 		double a = (Kp*A*Vrange*Vdig)/scaleHalf;	// Apply equation and obtain the weight in floating point format;
 		int WeightTemp = (int) Waccu*(a*Wmax/Vref);	// Amplifier the value to remove floating point and get an integer number;
 
@@ -111,60 +99,50 @@ public:
 		else if(abs(error) > 3000 && abs(error) < 5000) {
 			beta = betaV[WeightIndex][6];
 		}
-		else if(abs(error) > 5000 && abs(error) < 10000)
-		{
-		beta = betaV[WeightIndex][7];
+		else if(abs(error) > 5000 && abs(error) < 10000) {
+			beta = betaV[WeightIndex][7];
 		}
-		else if(abs(error) > 10000 && abs(error) < 20000)
-		{
-		beta = betaV[WeightIndex][8];
+		else if(abs(error) > 10000 && abs(error) < 20000) {
+			beta = betaV[WeightIndex][8];
 		}
-		else if(abs(error) > 20000 && abs(error) < 25000)
-		{
-		beta = betaV[WeightIndex][9];
+		else if(abs(error) > 20000 && abs(error) < 25000) {
+			beta = betaV[WeightIndex][9];
 		}
-		else
-		{
-		beta = betaV[WeightIndex][9];		// suppose to beta = 1.00;
+		else {
+			beta = betaV[WeightIndex][9];		// suppose to beta = 1.00;
 		}
 
-		if(beta == 1.0)
-		{
-		for(int i=1; i<nWeight;i++)
-		{
-		signalVect[i] = signal;
-		//			WeightVect[i] = WeightTemp;
+		if(beta == 1.0) {
+			for(int i=1; i<nWeight;i++) {
+				signalVect[i] = signal;
+				// WeightVect[i] = WeightTemp;
+			}
+			// Weight = WeightTemp;
 		}
-		//		Weight = WeightTemp;
-		}
-		else
-		{
-		//	int Wsum = 0;
-		for(int i=(nWeight-1);i>0;i--)
-		{
-		signalVect[i] = signalVect[i-1];
-		//		WeightVect[i] = WeightVect[i-1];
-		//		Wsum+= WeightVect[i];
-		}
-		signalVect[0] = signal;
-		//	WeightVect[0] = WeightTemp;
-		//	Wsum+= WeightVect[0];
-		//	Weight = Wsum/nWeight;
+		else {
+			//	int Wsum = 0;
+			for(int i=(nWeight-1);i>0;i--) {
+				signalVect[i] = signalVect[i-1];
+				// WeightVect[i] = WeightVect[i-1];
+				// Wsum+= WeightVect[i];
+			}
+			signalVect[0] = signal;
+			//	WeightVect[0] = WeightTemp;
+			//	Wsum+= WeightVect[0];
+			//	Weight = Wsum/nWeight;
 		}
 
-		if(abs(error) < 1000)	// If weight found is less then stabWeight we take stable weight
-		{
-		stable = 1;
-		WeightIndex = 1;
+		if(abs(error) < 1000) { // If weight found is less then stabWeight we take stable weight
+			stable = 1;
+			WeightIndex = 1;
 		}
-		else if((abs(error) > 20000) && stable == 1) // else, if goes bigger than unstWeight we don't have the weight yet
-		{
+		else if((abs(error) > 20000) && stable == 1) { // else, if goes bigger than unstWeight we don't have the weight yet
 		//		convCount++;
 		//		if(convCount > 500)
 		//		{
 		//			convCount = 0;
-		stable = 0;
-		WeightIndex = 0;
+			stable = 0;
+			WeightIndex = 0;
 		//		}
 		}
 
@@ -172,15 +150,20 @@ public:
 
 		return Weight;
 }
+	void offset(int _offset) {
+		offset_ = _offset;
+	}
+	int offset(void) {
+		return offset_;
+	}
 
 private:
 	HX711 hx711_;
+	int offset_ = 0;							// offset after tare;
 };
 
-//#define pin_data_HX711	32
-//#define pin_sck_HX711		31
-//#define pin_tare_HX711	30
-//#define pin_led			1
+#endif /* __LOAD_CELL_H_ */
+
 //void LOAD_CELL::tareSystem()
 //{
 //	int i, n = 100;
@@ -234,5 +217,3 @@ private:
 // //	glcd_big_str(0,2,Serial.buffer);
 // //	glcd_put_string(70,4," g");
 // }
-
-#endif /* __LOAD_CELL_H_ */
