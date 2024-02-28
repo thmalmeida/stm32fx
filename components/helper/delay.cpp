f8ed51925169daf2570b986f47d86b956e472ff9
thmalmeida <thmalmeida@gmail.com>
2024-02-27 22:55:46 -0300

    fixed TIM4 for us delay due local class declaration

--- components/helper/delay.cpp
+++ components/helper/delay.cpp
@@ -19,20 +19,32 @@
 // static TIM_DRIVER timer_us(3, 0.001, timer_mode::timer_counter);
 // static bool tim1_state = false;
 
-static TIM_DRIVER *timer_us;
+// static TIM_DRIVER *timer_us;
+
+// static TIM_DRIVER timer_us(4, 1/0.002, timer_mode::timer_counter);
 
 void delay_init(void) {
 	TIM_DRIVER timer0(4, 1/0.002, timer_mode::timer_counter);
-	timer_us = &timer0;
+	// timer_us = &timer0;
+
+	printf("delay timer init\n");
 }
 void delay_ms(uint32_t milliseconds) {
 	HAL_Delay(milliseconds);		// stm32
 	// sys_delay_ms(milliseconds);	// esp32
 }
 void delay_us(uint32_t microseconds) {
-	timer_us->reset_cnt();
-	while(timer_us->get_cnt() < 24*microseconds);
-
+	// timer_us.reset_cnt();
+	TIM4->CNT = 0;
+	// printf("V:%lu, cnt:%lu\n", 24*microseconds, timer_us.get_cnt());
+	// while(timer_us.get_cnt() < 24*microseconds) {
+	while(TIM4->CNT <24*microseconds);
+		// printf("cnt:%lu\n",TIM4->CNT);
+		// if(c++ > 1000000) {
+		// 	printf("FOUND!\n");
+		// 	break;
+		// }
+	// }
 	// another way
 	// if(tim1_state == false) {
 	// 	tim1_state = true;
@@ -41,8 +53,8 @@ void delay_us(uint32_t microseconds) {
 	// }
 
 	// // dummy way;
-	// uint32_t a;
-	// for(uint32_t i=0; i<microseconds	; i++) {
+	// uint32_t a=0;
+	// for(uint32_t i=0; i<10*microseconds; i++) {
 	// 	a++;
 	// }
 }
